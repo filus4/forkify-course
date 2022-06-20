115b145bc32397ed5c07e95cbf45ce0dfc9f44c7
filus4 <filej1025@gmail.com>
2022-06-20 16:17:19 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '92223f9c-45a8-4a88-ba41-c8c7304b1b8f';
+export const MODAL_CLOSE_SEC = 2.5;
